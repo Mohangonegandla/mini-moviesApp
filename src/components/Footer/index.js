ba6baeace6191563280943b6a919d62a55024c3d
mohangonegandla <tarakamohans@gmail.com>
2022-04-29 07:45:05 +0000

    MOVIESAPP

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import {FaGoogle, FaTwitter, FaYoutube, FaInstagram} from 'react-icons/fa'
+import MovieContext from '../../context/MovieContext'
+
+import './index.css'
+
+const Footer = () => (
+  <MovieContext.Consumer>
+    {value => {
+      const {username} = value
+      console.log(username)
+
+      return (
+        <div className="footer-container">
+          <div>
+            <FaGoogle size={30} className="footer-icon" />
+            <FaTwitter size={30} className="footer-icon" />
+            <FaYoutube size={30} className="footer-icon" />
+            <FaInstagram size={30} className="footer-icon" />
+          </div>
+          <p className="contact-heading">Contact us</p>
+        </div>
+      )
+    }}
+  </MovieContext.Consumer>
+)
+export default Footer
